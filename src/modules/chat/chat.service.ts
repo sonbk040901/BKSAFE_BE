457c69bac33a29ad50a04138f4da54a0888060be
sonbk040901 <leducson007@gmail.com>
2024-07-05 00:47:01 +0700

    update

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -28,7 +28,7 @@ export class ChatService {
       isDriver: 0 | 1;
       [key: string]: any;
     }[] = await this.chatRepository.query(
-      'SELECT c.id chatId, message, c.created_at createdAt, user_id userId, driver_id driverId, is_driver isDriver, d.*  FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY driver_id ORDER BY created_at DESC) as row_num FROM chats WHERE user_id = ?) AS c inner join drivers d on c.driver_id = d.id WHERE row_num = 1 order by c.created_at desc',
+      'SELECT c.id chatId, message, c.created_at createdAt, user_id userId, driver_id driverId, is_driver isDriver, d.*  FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY driver_id ORDER BY created_at asc) as row_num FROM chats WHERE user_id = ?) AS c inner join drivers d on c.driver_id = d.id WHERE row_num = 1 order by c.created_at desc',
       [userId],
     );
     return result.map((r) => {
@@ -75,7 +75,7 @@ export class ChatService {
       isDriver: 0 | 1;
       [key: string]: any;
     }[] = await this.chatRepository.query(
-      'SELECT c.id chatId, message, c.created_at createdAt, user_id userId, driver_id driverId, is_driver isDriver, d.*  FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY driver_id ORDER BY created_at DESC) as row_num FROM chats WHERE driver_id = ?) AS c inner join users u on c.user_id = u.id WHERE row_num = 1 order by c.created_at desc',
+      'SELECT c.id chatId, message, c.created_at createdAt, user_id userId, driver_id driverId, is_driver isDriver, d.*  FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY driver_id ORDER BY created_at asc) as row_num FROM chats WHERE driver_id = ?) AS c inner join users u on c.user_id = u.id WHERE row_num = 1 order by c.created_at desc',
       [driverId],
     );
     return result.map((r) => {
